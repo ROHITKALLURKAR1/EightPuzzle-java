c8a56086e2d5d960ee753615d0e84e77452c7e6c
rohit kallurkar <sam7499800354@gmail.com>
2026-07-24 23:27:12 +0530

    added shuffle button with workings

--- EightPuzzle.java
+++ EightPuzzle.java
@@ -2,18 +2,33 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 
 public class EightPuzzle extends  JFrame implements ActionListener{
     JButton b1,b2,b3,b4,b5,b6,b7,b8,b9;
 
+    JButton shuffle;
+    boolean shuffling = false;
 
 
     EightPuzzle(){
         setTitle("8 PUZZLE ");
         setSize(800, 800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new GridLayout(3, 3, 5, 5));
+        setLayout(new BorderLayout());
+
+        shuffle = new JButton("Shuffle");
+        
+        add(shuffle, BorderLayout.SOUTH);
+        
+        shuffle.addActionListener(this);
+        shuffle.setFont(new Font("Arial", Font.PLAIN, 30));
+
+        JPanel puzzlePanel = new JPanel();
+
+        puzzlePanel.setLayout(new GridLayout(3, 3, 5, 5));
+       // setLayout(new GridLayout(3, 3, 5, 5));
 
         b1 = new JButton("1");
         b2 = new JButton("2");
@@ -60,16 +75,25 @@ public class EightPuzzle extends  JFrame implements ActionListener{
 
        
 
-
-        add(b1);
-        add(b2);
-        add(b3);
-        add(b4);
-        add(b5);
-        add(b6);
-        add(b7);
-        add(b9);
-        add(b8);
+        puzzlePanel.add(b1);
+        puzzlePanel.add(b2);
+        puzzlePanel.add(b3);
+        puzzlePanel.add(b4);
+        puzzlePanel.add(b5);
+        puzzlePanel.add(b6);
+        puzzlePanel.add(b7);
+        puzzlePanel.add(b9);
+        puzzlePanel.add(b8);
+        add(puzzlePanel, BorderLayout.CENTER);
+        // add(b1);
+        // add(b2);
+        // add(b3);
+        // add(b4);
+        // add(b5);
+        // add(b6);
+        // add(b7);
+        // add(b9);
+        // add(b8);
 
 
        b1.addActionListener(this);
@@ -88,6 +112,10 @@ public class EightPuzzle extends  JFrame implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e){
 
+       if (e.getSource() == shuffle) {
+    shufflePuzzle();
+    return;
+    }
 
        if (e.getSource() == b5) {
 
@@ -215,13 +243,85 @@ if (e.getSource() == b9) {
 
 }
 
+   public void shufflePuzzle() {
 
-    public void move(JButton clicked, JButton empty) {
+    shuffling = true;
+
+    // Enable buttons again in case player already won
+    b1.setEnabled(true);
+    b2.setEnabled(true);
+    b3.setEnabled(true);
+    b4.setEnabled(true);
+    b5.setEnabled(true);
+    b6.setEnabled(true);
+    b7.setEnabled(true);
+    b8.setEnabled(true);
+    b9.setEnabled(true);
+
+    Random random = new Random();
+
+    for (int i = 0; i < 100; i++) {
+
+        if (b1.getText().equals(" ")) {
+            JButton[] moves = {b2, b4};
+            move(moves[random.nextInt(moves.length)], b1);
+        }
+
+        else if (b2.getText().equals(" ")) {
+            JButton[] moves = {b1, b3, b5};
+            move(moves[random.nextInt(moves.length)], b2);
+        }
+
+        else if (b3.getText().equals(" ")) {
+            JButton[] moves = {b2, b6};
+            move(moves[random.nextInt(moves.length)], b3);
+        }
+
+        else if (b4.getText().equals(" ")) {
+            JButton[] moves = {b1, b5, b7};
+            move(moves[random.nextInt(moves.length)], b4);
+        }
+
+        else if (b5.getText().equals(" ")) {
+            JButton[] moves = {b2, b4, b6, b9};
+            move(moves[random.nextInt(moves.length)], b5);
+        }
+
+        else if (b6.getText().equals(" ")) {
+            JButton[] moves = {b3, b5, b8};
+            move(moves[random.nextInt(moves.length)], b6);
+        }
+
+        else if (b7.getText().equals(" ")) {
+            JButton[] moves = {b4, b9};
+            move(moves[random.nextInt(moves.length)], b7);
+        }
+
+        else if (b9.getText().equals(" ")) {
+            JButton[] moves = {b7, b5, b8};
+            move(moves[random.nextInt(moves.length)], b9);
+        }
+
+        else if (b8.getText().equals(" ")) {
+            JButton[] moves = {b6, b9};
+            move(moves[random.nextInt(moves.length)], b8);
+        }
+    }
+
+    shuffling = false;
+}
+
+
+   public void move(JButton clicked, JButton empty) {
 
     if (empty.getText().equals(" ")) {
+
         empty.setText(clicked.getText());
         clicked.setText(" ");
-        checkwin();
+
+        if (!shuffling) {
+            checkwin();
+        }
     }
 }
 
@@ -260,6 +360,8 @@ public void checkwin(){
              
         }
 }
+
+
     
     public static void main(String[] args) {
        new EightPuzzle(); 
